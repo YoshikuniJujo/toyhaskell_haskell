35c745f71934a8da0a1c8e3300377ed9adc72609
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-08-17 10:19:20 +0900

    とりあえず四則演算はできるようになった。
    Parsec の TokenParser を使ってみようかな。

--- toy.hs
+++ toy.hs
@@ -1,53 +1,42 @@
 module Main where
 
-import System.IO
-import System.Environment
 import Control.Monad.Tools
 import Control.Monad
+import System.IO
 import Text.ParserCombinators.Parsec
-import Data.Either
-
-fromRight ( Right x ) = x
+import Data.Maybe
 
 main :: IO ()
 main = do
-	[ fn ] <- getArgs
-	parsed <- liftM ( map ( fromRight . parse parseDef "toy" ) . lines ) $
-		readFile fn
-	let pairs = map parsedToPair parsed
 	doWhile_ $ do
 		input <- prompt "toy> "
-		maybe ( putStrLn $ "Not in scope: `" ++ input ++ "'" ) ( print ) $
-			lookup input pairs
-		return $ input /= ":quit"
-
-parsedToPair :: Parsed -> ( String, String )
-parsedToPair ( PDef ( PSymbol sym ) ( PString str ) ) = ( sym, str )
-
+		when ( head input /= ':' ) $
+			putStrLn $ either show show $ parse parser "toy" input
+		return $ input `notElem` [ ":quit", ":q" ]
+	putStrLn "Leaving toyhaskell."
+	
 prompt :: String -> IO String
 prompt p = putStr p >> hFlush stdout >> getLine
 
-data Code = Apply Code Code | Value deriving Show
+parser :: Parser Int
+parser = do
+	x <- liftM read $ many1 digit
+	f <- parser'
+	eof
+	return $ f x
 
-data Value = Int Int | Primitive deriving Show
+parser' :: Parser ( Int -> Int )
+parser' = do	spaces
+		op <- oneOf "+-*"
+		spaces
+		y <- liftM read $ many1 digit
+		f <- parser'
+		return $ \x -> fromJust ( lookup op opList ) ( f x ) y
+	<|> return id
 
-data Primitive = PrimitiveIntIntInt ( Int -> Int -> Int )
-
-instance Show Primitive where
-	show _ = "<primitive>"
-
-data Parsed = PDef PPat PBody deriving Show
-
-data PPat = PSymbol String deriving Show
-data PBody = PString String deriving Show
-
-parseDef :: Parser Parsed
-parseDef = do
-	sym <- many lower
-	spaces
-	char '='
-	spaces
-	char '"'
-	body <- many $ noneOf "\""
-	char '"'
-	return $ PDef ( PSymbol sym ) ( PString body )
+opList :: [ ( Char, Int -> Int -> Int ) ]
+opList = [
+	( '+', (+) ),
+	( '-', (-) ),
+	( '*', (*) )
+ ]
