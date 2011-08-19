33161211526a38649830e884e8e05231bbcfcb4e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-08-19 10:41:23 +0900

    refactor Eval.hs finished

--- src/Eval.hs
+++ src/Eval.hs
@@ -18,6 +18,7 @@ eval env ( Apply f a )		= case eval env f of
 		eval ( setPatToEnv pat ( eval env a ) lenv `addEnvs` env ) body
 	Lambda lenv ( pat : pats ) body	->
 		Lambda ( setPatToEnv pat ( eval env a ) lenv ) pats body
+	e@( Error _ )			-> e
 	_				-> notFunctionError f
 eval env ( Letin pats body )	= eval ( setPatsToEnv pats env ) body
 eval env ( If test thn els )	= case eval env test of
@@ -53,57 +54,53 @@ initEnv = flip setsToEnv emptyEnv [
 	( "-", mkBinIntFunction (-) ),
 	( "*", mkBinIntFunction (*) ),
 	( "^", mkBinIntFunction (^) ),
-	( "putChar", Function putCharFun ),
 	( "==", mkIntCompFunction (==) ),
 	( ":", makeListFun ),
 	( ">>", concatMonadFun ),
-	( "return", returnFun )
+	( "return", returnFun ),
+	( "putChar", Function putCharFun )
  ]
 
 mkBinIntFunction :: ( Integer -> Integer -> Integer ) -> Value
 mkBinIntFunction op = Function fun
 	where
 	fun ( Integer x ) = let
-		funN vy =
-			case vy of
-				Integer y -> Integer $ x `op` y
-				_ -> Error "bad type" in
+		funN ( Integer y )	= Integer $ x `op` y
+		funN ni			= notMatchTypeError "Integer" ni in
 		Function funN
-	fun _ = Error "bad type"
+	fun ni = notMatchTypeError "Integer" ni
 
 mkIntCompFunction :: ( Integer -> Integer -> Bool ) -> Value
 mkIntCompFunction p = Function fun
 	where
 	fun ( Integer x ) = let
-		funN vy =
-			case vy of
-				Integer y -> if x `p` y
-					then Complex "True" [ ]
-					else Complex "False" [ ]
-				_ -> Error "bad type" in
+		funN ( Integer y )	= if x `p` y
+			then Complex "True" [ ]
+			else Complex "False" [ ]
+		funN ni			= notMatchTypeError "Integer" ni in
 		Function funN
-	fun _ = Error "bad type"
+	fun ni = notMatchTypeError "Integer" ni
+
+makeListFun :: Value
+makeListFun = Function fun
+	where fun v = let funV lst = Complex ":" [ v, lst ] in Function funV
+
+concatMonadFun :: Value
+concatMonadFun = Function fun
+	where
+	fun ( IOAction act1 ) = let
+		funA ( IOAction act2 ) = IOAction $ act1 >> act2
+		funA v = notMatchTypeError "IO" v in
+		Function funA
+	fun v = notMatchTypeError "IO" v
+
+returnFun :: Value
+returnFun = Function $ IOAction . return
 
 putCharFun :: Value -> Value
 putCharFun ( Char c ) = IOAction $ putChar c >> return Nil
 putCharFun v = Error $ "putChar :: String -> IO (): " ++ show v
 
-makeListFun :: Value
-makeListFun = Function fun
-	where
-	fun v = let funV lst = Complex ":" [ v, lst ] in
-		Function funV
-
-concatMonadFun :: Value
-concatMonadFun = Function fun
-	where
-	fun ( IOAction act1 ) =
-		let	funA ( IOAction act2 ) = IOAction $ act1 >> act2
-			funA v = Error $ "not IOAction: " ++ show v in
-			Function funA
-	fun v = Error $ "not IOAction: " ++ show v
-
-returnFun :: Value
-returnFun = Function fun
-	where
-	fun v = IOAction $ return v
+notMatchTypeError :: String -> Value -> Value
+notMatchTypeError typ val = Error $
+	"Couldn't match expected type `" ++ typ ++ "': " ++ show val
