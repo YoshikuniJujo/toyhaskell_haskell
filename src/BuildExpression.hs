2b7615f9ab717131f085475aa3212ad8bed292b6
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-08-23 13:06:01 +0900

    refactoring: BuildExpression not yet finished

--- src/BuildExpression.hs
+++ src/BuildExpression.hs
@@ -1,72 +1,39 @@
 module BuildExpression (
 	buildExprParser,
-	buildExprParser',
-	OpTable, Op,
+	Op,
 	Assoc( .. )
 ) where
 
 import Text.ParserCombinators.Parsec hiding ( token )
-import qualified Text.ParserCombinators.Parsec as P
-import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Expr (
+	buildExpressionParser, OperatorTable, Operator( .. ), Assoc( .. ) )
 
-type OpTable t a	= [ ( t, a -> a -> a, Int, Assoc ) ]
-type Op t a		= ( t, a -> a -> a, Int, Assoc )
+type Op t a	= ( t, a -> a -> a, Int, Assoc )
+type Token t st	= ( t -> Maybe () ) -> GenParser t st ()
+type Compare t	= t -> t -> Bool
 
-mkTableGen :: Eq b => ( t -> String ) -> ( t -> SourcePos ) -> ( t -> b ) ->
-	[ ( t, a -> a -> a, Int, Assoc ) ] -> OperatorTable t st a
-mkTableGen s p pre = foldr ( uncurry4 $ insertToTableGen s p pre ) $ replicate 10 [ ]
-
-mkTableGen' ::
-	( ( t -> Maybe () ) -> GenParser t st () ) -> ( t -> t -> Bool ) ->
-	[ ( t, a -> a -> a, Int, Assoc ) ] -> OperatorTable t st a
-mkTableGen' tk eq = foldr ( uncurry4 $ insertToTableGen' tk eq ) $ replicate 10 [ ]
-
-buildExprParser' :: Eq b => ( t -> String ) -> ( t -> SourcePos ) -> ( t -> b ) ->
-	[ ( t, a -> a -> a, Int, Assoc ) ] -> GenParser t st a ->
+buildExprParser :: Token t st -> Compare t -> [ Op t a ] -> GenParser t st a ->
 	GenParser t st a
-buildExprParser' s p pre tbl = buildExpressionParser ( mkTableGen s p pre tbl )
+buildExprParser tk eq tbl = buildExpressionParser ( mkTable tk eq tbl )
 
-buildExprParser ::
-	( ( t -> Maybe () ) -> GenParser t st () ) -> ( t -> t -> Bool ) ->
-	[ ( t, a -> a -> a, Int, Assoc ) ] -> GenParser t st a -> GenParser t st a
-buildExprParser tk eq tbl = buildExpressionParser ( mkTableGen' tk eq tbl )
+mkTable :: Token t st -> Compare t -> [ Op t a ] -> OperatorTable t st a
+mkTable tk eq = foldr ( uncurry4 $ insertToTable tk eq ) $ replicate 10 [ ]
 
 uncurry4 :: ( a -> b -> c -> d -> e ) -> ( a, b, c, d ) -> e
 uncurry4 f ( x, y, z, w ) = f x y z w
 
-insertToTableGen :: Eq b => ( t -> String ) -> ( t -> SourcePos ) -> ( t -> b ) ->
-	t -> ( a -> a -> a ) -> Int -> Assoc -> OperatorTable t st a ->
-	OperatorTable t st a
-insertToTableGen s p pre op f power assoc tbl =
+insertToTable :: Token t st -> Compare t -> t -> ( a -> a -> a ) -> Int ->
+	Assoc -> OperatorTable t st a -> OperatorTable t st a
+insertToTable tk eq op f power assoc tbl =
 	take pos tbl ++ [ fx : tbl !! pos ] ++ drop ( pos + 1 ) tbl
 	where
 	pos = 9 - power
-	fx = Infix ( mkOpParserGen s p pre op f ) assoc
+	fx = Infix ( mkOpParser tk eq op f ) assoc
 
-mkOpParserGen :: Eq b => ( t -> String ) -> ( t -> SourcePos ) -> ( t -> b ) ->
-	t -> ( a -> a -> a ) -> GenParser t st ( a -> a -> a )
-mkOpParserGen s p pre op f = P.token s p ( eqM ( pre op ) . pre ) >> return f
-
-insertToTableGen' ::
-	( ( t -> Maybe () ) -> GenParser t st () ) ->
-	( t -> t -> Bool ) -> t -> ( a -> a -> a ) -> Int -> Assoc ->
-	OperatorTable t st a -> OperatorTable t st a
-insertToTableGen' tk eq op f power assoc tbl =
-	take pos tbl ++ [ fx : tbl !! pos ] ++ drop ( pos + 1 ) tbl
-	where
-	pos = 9 - power
-	fx = Infix ( mkOpParserGen' tk eq op f ) assoc
-
-mkOpParserGen' ::
-	( ( t -> Maybe () ) -> GenParser t st () ) ->
-	( t -> t -> Bool ) -> t -> ( a -> a -> a ) ->
+mkOpParser :: Token t st -> Compare t -> t -> ( a -> a -> a ) ->
 	GenParser t st ( a -> a -> a )
-mkOpParserGen' tk eq op f = tk ( boolM . eq op ) >> return f
+mkOpParser tk eq op f = tk ( boolM . eq op ) >> return f
 
 boolM :: Bool -> Maybe ()
 boolM True	= Just ()
 boolM False	= Nothing
-
-eqM :: Eq a => a -> a -> Maybe ()
-eqM x y	| x == y	= Just ()
-	| otherwise	= Nothing
