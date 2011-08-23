2c0e5431f8e7bf6c3881f3e5465d2486bc60fa73
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-08-23 19:45:31 +0900

    refactoring: BuildExpression finished

--- src/BuildExpression.hs
+++ src/BuildExpression.hs
@@ -4,36 +4,33 @@ module BuildExpression (
 	Assoc( .. )
 ) where
 
-import Text.ParserCombinators.Parsec hiding ( token )
+import Text.ParserCombinators.Parsec ( GenParser )
 import Text.ParserCombinators.Parsec.Expr (
 	buildExpressionParser, OperatorTable, Operator( .. ), Assoc( .. ) )
 
-type Op t a	= ( t, a -> a -> a, Int, Assoc )
+type Op t a	= ( t, OpFun a, Int, Assoc )
+type OpFun a	= a -> a -> a
 type Token t st	= ( t -> Maybe () ) -> GenParser t st ()
 type Compare t	= t -> t -> Bool
+type P		= GenParser
+type T t st a	= OperatorTable t st a
 
-buildExprParser :: Token t st -> Compare t -> [ Op t a ] -> GenParser t st a ->
-	GenParser t st a
+buildExprParser :: Token t st -> Compare t -> [ Op t a ] -> P t st a -> P t st a
 buildExprParser tk eq tbl = buildExpressionParser ( mkTable tk eq tbl )
 
-mkTable :: Token t st -> Compare t -> [ Op t a ] -> OperatorTable t st a
-mkTable tk eq = foldr ( uncurry4 $ insertToTable tk eq ) $ replicate 10 [ ]
+mkTable :: Token t st -> Compare t -> [ Op t a ] -> T t st a
+mkTable tk eq = foldr ( insertToTable tk eq ) $ replicate 10 [ ]
 
-uncurry4 :: ( a -> b -> c -> d -> e ) -> ( a, b, c, d ) -> e
-uncurry4 f ( x, y, z, w ) = f x y z w
+insertToTable :: Token t st -> Compare t -> Op t a -> T t st a -> T t st a
+insertToTable tk eq ( op, f, power, assoc ) tbl =
+	insertToI ( 9 - power ) ( Infix ( mkOpParser tk eq op f ) assoc ) tbl
 
-insertToTable :: Token t st -> Compare t -> t -> ( a -> a -> a ) -> Int ->
-	Assoc -> OperatorTable t st a -> OperatorTable t st a
-insertToTable tk eq op f power assoc tbl =
-	take pos tbl ++ [ fx : tbl !! pos ] ++ drop ( pos + 1 ) tbl
-	where
-	pos = 9 - power
-	fx = Infix ( mkOpParser tk eq op f ) assoc
-
-mkOpParser :: Token t st -> Compare t -> t -> ( a -> a -> a ) ->
-	GenParser t st ( a -> a -> a )
+mkOpParser :: Token t st -> Compare t -> t -> OpFun a -> P t st ( OpFun a )
 mkOpParser tk eq op f = tk ( boolM . eq op ) >> return f
 
 boolM :: Bool -> Maybe ()
 boolM True	= Just ()
 boolM False	= Nothing
+
+insertToI :: Int -> a -> [ [ a ] ] -> [ [ a ] ]
+insertToI i x xs = take i xs ++ [ x : xs !! i ] ++ drop ( i + 1 ) xs
