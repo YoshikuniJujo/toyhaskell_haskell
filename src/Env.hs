9864938cfb8087c59bbd28a0312100eff078022a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-08-25 11:27:02 +0900

    refactoring: finished

--- src/Env.hs
+++ src/Env.hs
@@ -8,7 +8,7 @@ module Env (
 	addEnvs
 ) where
 
-import Data.Maybe
+import Data.Maybe ( listToMaybe )
 
 data Env p v = Env [ ( [ String ], p, v ) ]
 
@@ -16,15 +16,13 @@ emptyEnv :: Env p v
 emptyEnv = Env [ ]
 
 setToEnv :: ( String -> p ) -> String -> v -> Env p v -> Env p v
-setToEnv pv var val ( Env ps ) =
-	Env $ ( [ var ], pv var, val ) : ps
+setToEnv pv var val ( Env ps ) = Env $ ( [ var ], pv var, val ) : ps
 
 setsToEnv :: ( String -> p ) -> [ ( String, v ) ] -> Env p v -> Env p v
 setsToEnv pv = flip $ foldr $ uncurry ( setToEnv pv )
 
 setPatToEnv :: ( p -> [ String ] ) -> p -> v -> Env p v -> Env p v
-setPatToEnv gpv pat val ( Env env ) = Env $ ( vars, pat, val ) : env
-	where vars = gpv pat
+setPatToEnv gpv pat val ( Env env ) = Env $ ( gpv pat, pat, val ) : env
 
 setPatsToEnv :: ( p -> [ String ] ) -> [ ( p, v ) ] -> Env p v -> Env p v
 setPatsToEnv gpv = flip $ foldr $ uncurry ( setPatToEnv gpv )
@@ -32,11 +30,9 @@ setPatsToEnv gpv = flip $ foldr $ uncurry ( setPatToEnv gpv )
 getFromEnv :: ( v -> p -> Maybe [ ( String, v ) ] ) -> ( v -> v ) -> String ->
 	Env p v -> Maybe v
 getFromEnv m f var ( Env ps ) = do
-	( _, pat, val ) <- usePat
+	( _, pat, val ) <- listToMaybe $ filter ( ( var `elem` ) . one ) ps
 	m ( f val ) pat >>= lookup var
-	where
-	one ( x, _, _ ) = x
-	usePat = listToMaybe $ filter ( ( var `elem` ) . one ) ps
+	where one ( x, _, _ ) = x
 
 addEnvs :: Env p v -> Env p v -> Env p v
 addEnvs ( Env ps1 ) ( Env ps2 ) = Env $ ps1 ++ ps2
