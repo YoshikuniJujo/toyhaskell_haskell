9019b80b170de2a1a6aef4593206f915254ced1d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-02 16:36:46 +0900

    refactoring: Alpha not yet finished

--- src/Alpha.hs
+++ src/Alpha.hs
@@ -2,100 +2,104 @@
 
 module Alpha ( alpha ) where
 
-import Value
-import Data.List
-import Control.Arrow
+import Value ( Value( .. ), Pattern( .. ), getPatVars )
+import Data.List ( intersect, union )
+import Control.Arrow ( second, (***) )
+
+--------------------------------------------------------------------------------
 
 alpha :: Value -> Value
-alpha = makeVar . alpha_ [ ]
+alpha = mkVar . alpha_ [ ]
 
-makeVar :: Value -> Value
-makeVar ( Identifier var 0 )	= Identifier var 0
-makeVar ( Identifier var n )	= Identifier ( var ++ "~" ++ show n ) n
-makeVar ( Complex con vs )	= Complex con $ map makeVar vs
-makeVar ( Apply v1 v2 )		= Apply ( makeVar v1 ) ( makeVar v2 )
-makeVar ( Lambda ps v )		= Lambda ( map makeVarP ps ) ( makeVar v )
-makeVar ( Case v ps )		= Case ( makeVar v ) ( map makeVarPair ps )
-makeVar ( Letin ps v )		= Letin ( map makeVarPair ps ) ( makeVar v )
-makeVar ( Let ps )		= Let ( map makeVarPair ps )
-makeVar v			= v
-
-makeVarPair :: ( Pattern, Value ) -> ( Pattern, Value )
-makeVarPair ( p, v ) = ( makeVarP p, makeVar v )
-
-makeVarP :: Pattern -> Pattern
-makeVarP ( PatVar var 0 )	= PatVar var 0
-makeVarP ( PatVar var n )	= PatVar ( var ++ "~" ++ show n ) n
-makeVarP ( PatCon con pats )	= PatCon con ( map makeVarP pats )
-makeVarP p			= p
-
-alpha_ :: [ ( String, Int ) ] -> Value -> Value
-alpha_ used ( Lambda vs body )	=
-	Lambda ( map ( setNextsPat usedV ) vs ) $ alpha_ nused $
-		setNextsValue usedV body
+alpha_ :: [ String ] -> Value -> Value
+alpha_ pre ( Complex con mems )	= Complex con $ alpha_ pre `map` mems
+alpha_ pre ( Apply fun arg )	= Apply ( alpha_ pre fun ) $ alpha_ pre arg
+alpha_ pre ( Lambda args body )	= Lambda ( mapSuccVars dups args ) $
+	alpha_ ( pre `union` vars ) $ succVars dups body
 	where
-	usedV	= map fst used `intersect` concatMap getPatVars vs
-	nused	= updateUsed used ( concatMap getPatVars vs )
-alpha_ used ( Apply v1 v2 )	=
-	Apply ( alpha_ used v1 ) ( alpha_ used v2 )
-alpha_ used ( Complex con vs )	= Complex con $ map ( alpha_ used ) vs
-alpha_ used ( Case v ps )	=
-	Case ( alpha_ used v ) $ map ( alphaCase used ) ps
-alpha_ used ( Letin ps v )	=
-	Letin ( map ( second ( alpha_ nused ) . setNextsPair usedV ) ps ) $
-		alpha_ nused $ setNextsValue usedV v
+	vars	= getPatVars `concatMap` args
+	dups	= pre `intersect` vars
+alpha_ pre ( Case val bodies )	=
+	Case ( alpha_ pre val ) $ alphaPV pre `map` bodies
+alpha_ pre ( Letin defs body )	=
+	Letin ( map ( second ( alpha_ newPre ) . succVars dups ) defs ) $
+		alpha_ newPre $ succVars dups body
+--	Letin ( alphaPV pre `map` defs ) $ alpha_ newPre $ succVars dups body
 	where
-	usedV	= map fst used `intersect` concatMap getPatVars ( map fst ps )
-	nused	= updateUsed used ( concatMap getPatVars $ map fst ps )
-alpha_ used ( Let ps )		=
-	Let $ map ( second ( alpha_ nused ) . setNextsPair usedV ) ps
+	vars	= ( getPatVars . fst ) `concatMap` defs
+	dups	= pre `intersect` vars
+	newPre	= pre `union` vars
+alpha_ pre ( Let defs )		=
+	Let $ ( second ( alpha_ $ pre `union` vars ) .
+		succVars ( pre `intersect` vars ) ) `map` defs
 	where
-	usedV	= map fst used `intersect` concatMap getPatVars ( map fst ps )
-	nused	= updateUsed used ( concatMap getPatVars $ map fst ps )
+	vars	= ( getPatVars . fst ) `concatMap` defs
+	dups	= pre `intersect` vars
+	newPre	= pre `union` vars
+-- alpha_ pre ( Let defs )		= Let $ alphaPV pre `map` defs
 alpha_ _ v			= v
 
-alphaCase :: [ ( String, Int ) ] -> ( Pattern, Value ) -> ( Pattern, Value )
-alphaCase used ( pat, val )	=
-	( setNextsPat usedV pat, alpha_ nused $ setNextsValue usedV val )
-	where
-	usedV	= map fst used `intersect` getPatVars pat
-	nused	= updateUsed used ( getPatVars pat )
+alphaPV :: [ String ] -> ( Pattern, Value ) -> ( Pattern, Value )
+alphaPV pre body@( pat, _ )	= second ( alpha_ $ pre `union` vars ) $
+	succVars ( pre `intersect` vars ) body
+	where vars = getPatVars pat
 
-updateUsed :: [ ( String, Int ) ] -> [ String ] -> [ ( String, Int ) ]
-updateUsed [ ] vars = map ( , 1 ) vars
-updateUsed ( ( v, n ) : us ) vars
-	| v `elem` vars	= ( v, n + 1 ) : updateUsed us ( filter ( /= v ) vars )
-	| otherwise	= ( v, n ) : updateUsed us vars
+succVars :: Alpha sv => [ String ] -> sv -> sv
+succVars = flip $ foldr succVar
 
-setNextsValue :: [ String ] -> Value -> Value
-setNextsValue = flip $ foldr setNextValue
+mapSuccVars :: Alpha sv => [ String ] -> [ sv ] -> [ sv ]
+mapSuccVars = map . succVars
 
-setNextsPat :: [ String ] -> Pattern -> Pattern
-setNextsPat = flip $ foldr setNextPat
+class Alpha sv where
+	succVar	:: String -> sv -> sv
+	mkVar	:: sv -> sv
+
+instance Alpha Pattern where
+	succVar	= succVarPat
+	mkVar	= mkVarPat
+
+instance Alpha Value where
+	succVar	= setNextValue
+	mkVar	= mkVarVal
+
+instance ( Alpha a, Alpha b ) => Alpha ( a, b ) where
+	succVar vars	= succVar vars *** succVar vars
+	mkVar		= mkVar *** mkVar
 
 setNextValue :: String -> Value -> Value
 setNextValue v0 ( Lambda vs body )	=
-	Lambda ( map ( setNextPat v0 ) vs ) $ setNextValue v0 body
+	Lambda ( succVarPat v0 `map` vs ) $ setNextValue v0 body
 setNextValue v0 ( Identifier v1 n )
 	| v0 == v1			= Identifier v1 $ n + 1
 setNextValue v0 ( Apply v1 v2 )		=
 	Apply ( setNextValue v0 v1 ) ( setNextValue v0 v2 )
 setNextValue v0 ( Complex con vs )	=
-	Complex con $ map ( setNextValue v0 ) vs
+	Complex con $ setNextValue v0 `map` vs
 setNextValue v0 ( Case v1 ps )		=
-	Case ( setNextValue v0 v1 ) ( map ( setNextPair v0 ) ps )
+	Case ( setNextValue v0 v1 ) ( succVar v0 `map` ps )
 setNextValue v0 ( Letin ps body )	=
-	Letin ( map ( setNextPair v0 ) ps ) $ setNextValue v0 body
+	Letin ( succVar v0 `map` ps ) $ setNextValue v0 body
 setNextValue _ v			= v
 
-setNextsPair :: [ String ] -> ( Pattern, Value ) -> ( Pattern, Value )
-setNextsPair = flip $ foldr setNextPair
-
-setNextPair :: String -> ( Pattern, Value ) -> ( Pattern, Value )
-setNextPair v0 ( p, v ) = ( setNextPat v0 p, setNextValue v0 v )
-
-setNextPat :: String -> Pattern -> Pattern
-setNextPat v0 ( PatVar v1 n )
+succVarPat :: String -> Pattern -> Pattern
+succVarPat v0 ( PatVar v1 n )
 	| v0 == v1	= PatVar v1 $ n + 1
-setNextPat v0 ( PatCon c pats )	= PatCon c $ map ( setNextPat v0 ) pats
-setNextPat _ p		= p
+succVarPat v0 ( PatCon c pats )	= PatCon c $ succVarPat v0 `map` pats
+succVarPat _ p		= p
+
+mkVarVal :: Value -> Value
+mkVarVal ( Identifier var 0 )	= Identifier var 0
+mkVarVal ( Identifier var n )	= Identifier ( var ++ "~" ++ show n ) n
+mkVarVal ( Complex con mems )	= Complex con $ mkVar `map` mems
+mkVarVal ( Apply fun arg )		= Apply ( mkVar fun ) $ mkVar arg
+mkVarVal ( Lambda args body )	= Lambda ( mkVar `map` args ) $ mkVar body
+mkVarVal ( Case val bodies )	= Case ( mkVar val ) $ mkVar `map` bodies
+mkVarVal ( Letin defs body )	= Letin ( mkVar `map` defs ) $ mkVar body
+mkVarVal ( Let defs )		= Let $ mkVar `map` defs
+mkVarVal val			= val
+
+mkVarPat :: Pattern -> Pattern
+mkVarPat ( PatVar var 0 )	= PatVar var 0
+mkVarPat ( PatVar var n )	= PatVar ( var ++ "~" ++ show n ) n
+mkVarPat ( PatCon con pats )	= PatCon con $ mkVarPat `map` pats
+mkVarPat p			= p
