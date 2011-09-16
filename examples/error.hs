54391244f0706e4f8063640bd2b48332d726aacb
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-17 06:05:17 +0900

    tribial: edit memo and examples

---
+++ examples/error.hs
@@ -0,0 +1,5 @@
+module Main where
+
+hoge = id
+
+hage = 3
--- examples/ski.hs
+++ examples/ski.hs
@@ -3,3 +3,11 @@ module Main where
 i x = x
 k x y = x
 s x y z = ( x z ) ( y z )
+
+--
+
+twice = s s ( k i ) i
+
+f g h y = g ( h i y )
+
+two = s ( k twice ) f
