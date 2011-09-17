0a0be96b735e1d7744079ec90e30e5a69ef5dc7b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-17 09:33:57 +0900

    refactoring: Eval finished

--- src/Eval.hs
+++ src/Eval.hs
@@ -2,7 +2,7 @@ module Eval ( toyEval ) where
 
 import Value ( Value(
 	Var, Comp, App, Fun, Lambda, Closure, Case, Letin, Let, Module, Error ),
-	Pattern, match, Env, setVars, setPat, setPats, getVal, patVars )
+	Pattern, match, patVars, Env, setVars, setPat, setPats, getVal )
 import Data.Maybe ( fromMaybe )
 
 --------------------------------------------------------------------------------
@@ -13,45 +13,44 @@ toyEval env val = case noVars env val of
 	vars	-> Error $ unlines $ map errMsg vars
 	where errMsg var = "\tNot in scope: `" ++ var ++ "'"
 
-getV :: Env -> String -> Maybe Value
-getV env var = getVal ( eval env ) var env
+get :: Env -> String -> Maybe Value
+get env var = getVal ( eval env ) var env
 
 noVars :: Env -> Value -> [ String ]
-noVars env ( Var i _ )		= maybe [ i ] ( noVars env ) $ env `getV` i
+noVars env ( Var v _ )		= maybe [ v ] ( noVars env ) $ env `get` v
 noVars env ( App f a )		= noVars env f ++ noVars env a
-noVars env ( Lambda ps expr )	= ps `filterVars` noVars env expr
-noVars env ( Case key sels )	= noVars env key ++ noVarsC env `concatMap` sels
-noVars env ( Letin defs expr )	= map fst defs `filterVars`
-	( noVars env expr ++ ( noVars env . snd ) `concatMap` defs )
-noVars env ( Let defs )		= map fst defs `filterVars`
-	( ( noVars env . snd ) `concatMap` defs )
+noVars env ( Lambda ps ex )	= ps `filterVars` noVars env ex
+noVars env ( Case key sels )	= noVars env key ++ nvc `concatMap` sels
+	where nvc ( pat, ex ) = [ pat ] `filterVars` noVars env ex
+noVars env ( Letin defs ex )	=
+	map fst defs `filterVars` noVars env ex ++ noVars env ( Let defs )
+noVars env ( Let defs )		=
+	map fst defs `filterVars` ( noVars env . snd ) `concatMap` defs
 noVars env ( Module defs )	= noVars env ( Let defs )
 noVars _ _			= [ ]
 
+infixl	8 `filterVars`
 filterVars :: [ Pattern ] -> [ String ] -> [ String ]
 filterVars pats = filter ( `notElem` patVars `concatMap` pats )
 
-noVarsC :: Env -> ( Pattern, Value ) -> [ String ]
-noVarsC env ( test, expr ) = ( `notElem` patVars test ) `filter` noVars env expr
-
 eval :: Env -> Value -> Value
-eval env ( Var i _ )	= eval env $ fromMaybe ( noVar i ) $ getV env i
+eval env ( Var v _ )		= eval env $ fromMaybe ( noVar v ) $ env `get` v
 eval env ( Comp con mems )	= Comp con $ eval env `map` mems
 eval env ( App f a )		= case eval env f of
 	Fun fun			-> fun $ eval env a
-	Closure ce [ p ] b	-> eval ( setPat p ( eval env a ) ce ) b
-	Closure ce ( p : ps ) b	-> Closure ( setPat p ( eval env a ) ce ) ps b
+	Closure ce [ p ] e	-> eval ( setPat p ( eval env a ) ce ) e
+	Closure ce ( p : ps ) e	-> Closure ( setPat p ( eval env a ) ce ) ps e
 	err@( Error _ )		-> err
 	_			-> notFunction f
-eval env ( Lambda ps expr )	= Closure env ps expr
-eval env ( Case key sels )	= evalC env ( eval env key ) sels
-eval env ( Letin defs body )	= eval ( setPats defs env ) body
+eval env ( Lambda ps ex )	= Closure env ps ex
+eval env ( Case key sels )	= ec sels
+	where
+	ec [ ]			= nonExhaustive
+	ec ( ( pat, ex ) : r )	= let k = eval env key in
+		maybe ( ec r ) ( flip eval ex . flip setVars env ) $ match k pat
+eval env ( Letin defs ex )	= eval ( setPats defs env ) ex
 eval _ v			= v
 
-evalC :: Env -> Value -> [ ( Pattern, Value ) ] -> Value
-evalC _ _ [ ]				= nonExhaustive
-evalC env key ( ( test, expr ) : sels )	= maybe ( evalC env key sels )
-	( flip eval expr . flip setVars env ) $ match key test
 
 --------------------------------------------------------------------------------
 
