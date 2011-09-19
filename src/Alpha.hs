9c8862e783e05e273d4c0d20dfb36b2609b1a6e7
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-19 20:52:06 +0900

    refactoring: Alpha finished

--- src/Alpha.hs
+++ src/Alpha.hs
@@ -4,8 +4,7 @@ module Alpha ( alpha, alphaEnv ) where
 
 import Value (
 	Value( Var, Comp, App, Lambda, Case, Letin, Let, Module ),
-	Pattern( PatVar, PatCon ), patVars, Env, mapEnv,
-	Var( V ) )
+	Pattern( PatVar, PatCon ), patVars, Env, mapEnv, Var( V ) )
 import Data.List ( intersect, union )
 import Control.Arrow ( second, (***), (&&&) )
 
@@ -16,22 +15,22 @@ alphaEnv = flip ( foldr ae )  . ( patVars `concatMap` )
 	where ae v = mapEnv ( succV v ) ( succV v ) ( succV v )
 
 alpha :: [ String ] -> Value -> Value
-alpha pre ( Comp con mems )	= Comp con $ alpha pre `map` mems
-alpha pre ( App fun arg )	= App ( alpha pre fun ) $ alpha pre arg
-alpha pre ( Lambda ps ex )	= let ( dups, np ) = getDP pre ps in
-	Lambda ( map ( succVs dups ) ps ) $ alpha np $ succVs dups ex
-alpha pre ( Case key sels )	= Case ( alpha pre key ) $ ac `map` sels
-	where ac sel@( cond, _ ) = let ( dups, np ) = getDP pre [ cond ] in
+alpha pre ( Comp nm ms )	= Comp nm $ alpha pre `map` ms
+alpha pre ( App f a )		= App ( alpha pre f ) $ alpha pre a
+alpha pre ( Lambda ps e )	= let ( dups, np ) = getDP pre ps in
+	Lambda ( map ( succVs dups ) ps ) $ alpha np $ succVs dups e
+alpha pre ( Case k alts )	= Case ( alpha pre k ) $ ac `map` alts
+	where ac sel@( test, _ ) = let ( dups, np ) = getDP pre [ test ] in
 		second ( alpha np ) $ succVs dups sel
-alpha pre ( Letin defs ex )	= let ( dups, np ) = getDP pre $ map fst defs in
-	Letin ( alphaDefs pre defs ) $ alpha np $ succVs dups ex
-alpha pre ( Module defs )	= Module $ alphaDefs pre defs
-alpha pre ( Let defs )		= Let $ second ( alpha pre ) `map` defs
+alpha pre ( Letin ds e )	= let ( dups, np ) = getDP pre $ map fst ds in
+	Letin ( alphaDefs pre ds ) $ alpha np $ succVs dups e
+alpha pre ( Module ds )		= Module $ alphaDefs pre ds
+alpha pre ( Let ds )		= Let $ second ( alpha pre ) `map` ds
 alpha _ v			= v
 
 alphaDefs :: [ String ] -> [ ( Pattern, Value ) ] -> [ ( Pattern, Value ) ]
-alphaDefs pre defs		= let ( dups, np ) = getDP pre $ map fst defs in
-	( second ( alpha np ) . succVs dups ) `map` defs
+alphaDefs pre ds		= let ( dups, np ) = getDP pre $ map fst ds in
+	( second ( alpha np ) . succVs dups ) `map` ds
 
 getDP :: [ String ] -> [ Pattern ] -> ( [ String ], [ String ] )
 getDP = curry $
@@ -50,19 +49,19 @@ instance Alpha Var where
 
 instance Alpha Pattern where
 	succV v ( PatVar x n )
-		| v == x		= PatVar x $ n + 1
-	succV v ( PatCon c mems )	= PatCon c $ succV v `map` mems
-	succV _ p			= p
+		| v == x	= PatVar x $ n + 1
+	succV v ( PatCon c ms )	= PatCon c $ succV v `map` ms
+	succV _ p		= p
 
 instance Alpha Value where
-	succV v ( Lambda ps e )	= Lambda ( succV v `map` ps ) $ succV v e
 	succV v ( Var x n )
 		| v == x	= Var x $ n + 1
+	succV v ( Comp nm ms )	= Comp nm $ succV v `map` ms
 	succV v ( App f a )	= App ( succV v f ) ( succV v a )
-	succV v ( Comp c ms )	= Comp c $ succV v `map` ms
-	succV v ( Case k s )	= Case ( succV v k ) ( succV v `map` s )
+	succV v ( Lambda ps e )	= Lambda ( succV v `map` ps ) $ succV v e
+	succV v ( Case k alts )	= Case ( succV v k ) ( succV v `map` alts )
 	succV v ( Letin ds e )	= Letin ( succV v `map` ds ) $ succV v e
-	succV _ v		= v
+	succV _ val		= val
 
 instance ( Alpha a, Alpha b ) => Alpha ( a, b ) where
-	succV vars	= succV vars *** succV vars
+	succV vars = succV vars *** succV vars
