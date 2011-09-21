e1c73126577fad8d4c0e46df67d05981c3412e2c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-09-21 20:54:05 +0900

    refactoring: Preprocessor not yet finished

--- src/Preprocessor.hs
+++ src/Preprocessor.hs
@@ -12,73 +12,6 @@ import Control.Monad.State ( State, evalState, put, get, gets )
 
 --------------------------------------------------------------------------------
 
-prep :: ( Token -> Parse a ) -> Parse a
-prep = ( preprocessor >>= )
-
-preprocessor :: Parse Token
-preprocessor = do
-	t	<- addLayoutTokens
-	mm	<- peekIndent
-	case t of
-		Indent n	-> case mm of
-			Just m	| m == n	-> return $ Special ';'
-				| n < m		-> do
-					_ <- popIndent
-					pushBuf ( t, 0 )
-					return $ Special '}'
-			_			-> preprocessor
-		AddBrace n	-> case mm of
-			Just m	| n > m	-> pushIndent n >> return ( Special '{' )
-			Nothing	| n > 0	-> pushIndent n >> return ( Special '{' )
-			_		-> do	pushBuf ( Indent n, 0 )
-						pushBuf ( Special '}', 0 )
-						return $ Special '{'
-		Special '}'	-> case mm of
-			Just 0	-> popIndent >> return t
-			_	-> error "bad close brace"
-		Special '{'	-> pushIndent 0 >> return t
-		TokEOF		-> ( flip . maybe ) ( return t ) mm $ \_ -> do
-			_ <- popIndent
-			return $ Special '}'
-		_		-> return t
-
-addLayoutTokens :: Parse Token
-addLayoutTokens = do
-	( t, _ ) <- getToken
-	case t of
-		ReservedId res | res `elem` keywords	-> do
-			nt <- peekNextToken
-			case nt of
-				( Special '{', _ )	-> return ()
-				( TokEOF, _ )		->
-					pushBuf ( AddBrace 0, 0 )
-				( _, cs )		->
-					pushBuf ( AddBrace cs, 0 )
-			return t
-		NewLine					->
-			fmap ( Indent . snd ) peekNextToken
-		_					-> return t
-	where
-	keywords = [ "where", "let", "do", "of" ]
-
-peekNextToken :: Parse ( Token, Int )
-peekNextToken = lexerNoNL >>= \ret -> pushBuf ret >> return ret
-	where
-	lexerNoNL = getToken >>= \t -> case t of
-		( NewLine, _ )	-> lexerNoNL
-		_		-> return t
-
-getToken :: Parse ( Token, Int )
-getToken = popBuf >>= \mt -> ( flip . flip maybe ) return mt $ do
-	src	<- gets source
-	cs	<- gets cols
-	let ( t, fin, rest ) = lexeme lexer src
-	updatePos fin
-	putSrc rest
-	return ( t, cs )
-
---------------------------------------------------------------------------------
-
 type Parse = State ParseState
 
 data ParseState = ParseState {
@@ -144,3 +77,70 @@ peekIndent = do
 	case ma of
 		m : _	-> return $ Just m
 		_	-> return Nothing
+
+--------------------------------------------------------------------------------
+
+prep :: ( Token -> Parse a ) -> Parse a
+prep = ( preprocessor >>= )
+
+preprocessor :: Parse Token
+preprocessor = do
+	t	<- addLayoutTokens
+	mm	<- peekIndent
+	case t of
+		Indent n	-> case mm of
+			Just m	| m == n	-> return $ Special ';'
+				| n < m		-> do
+					_ <- popIndent
+					pushBuf ( t, 0 )
+					return $ Special '}'
+			_			-> preprocessor
+		AddBrace n	-> case mm of
+			Just m	| n > m	-> pushIndent n >> return ( Special '{' )
+			Nothing	| n > 0	-> pushIndent n >> return ( Special '{' )
+			_		-> do	pushBuf ( Indent n, 0 )
+						pushBuf ( Special '}', 0 )
+						return $ Special '{'
+		Special '}'	-> case mm of
+			Just 0	-> popIndent >> return t
+			_	-> error "bad close brace"
+		Special '{'	-> pushIndent 0 >> return t
+		TokEOF		-> ( flip . maybe ) ( return t ) mm $ \_ -> do
+			_ <- popIndent
+			return $ Special '}'
+		_		-> return t
+
+addLayoutTokens :: Parse Token
+addLayoutTokens = do
+	( t, _ ) <- getToken
+	case t of
+		ReservedId res | res `elem` keywords	-> do
+			nt <- peekNextToken
+			case nt of
+				( Special '{', _ )	-> return ()
+				( TokEOF, _ )		->
+					pushBuf ( AddBrace 0, 0 )
+				( _, cs )		->
+					pushBuf ( AddBrace cs, 0 )
+			return t
+		NewLine					->
+			fmap ( Indent . snd ) peekNextToken
+		_					-> return t
+	where
+	keywords = [ "where", "let", "do", "of" ]
+
+peekNextToken :: Parse ( Token, Int )
+peekNextToken = lexerNoNL >>= \ret -> pushBuf ret >> return ret
+	where
+	lexerNoNL = getToken >>= \t -> case t of
+		( NewLine, _ )	-> lexerNoNL
+		_		-> return t
+
+getToken :: Parse ( Token, Int )
+getToken = popBuf >>= \mt -> ( flip . flip maybe ) return mt $ do
+	src	<- gets source
+	cs	<- gets cols
+	let ( t, fin, rest ) = lexeme lexer src
+	updatePos fin
+	putSrc rest
+	return ( t, cs )
